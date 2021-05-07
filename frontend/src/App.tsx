83078e01d5c7320352357dd39378baa1d4833ad8
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-05-07 16:03:00 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable />   
+      </div>
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
+// No retorno da função que é o componente do React, pode ter apenas
+// um elemento e como incluímos <NavBar/> e <div><div/>, para delimitarmos 
+// com um elemento apenas, inserimos o fragment, que seria <> antes do 
+// elemento e </> fechando.
